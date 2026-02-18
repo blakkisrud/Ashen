9ec0189bfa81a61260533bb8eb8869794439b5c4
Johan Blakkisrud <blakkisrud@gmail.com>
2026-02-18 17:21:52 +0100

    Added package to fix

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     install_requires=[
         "click",
     ],
+    include_package_data=True,
+    package_data={"ashen": ["resources/FULL_RAD_LIST.RAD"]},
     entry_points={
         "console_scripts": [
             "ashen=ashen.cli:cli",
